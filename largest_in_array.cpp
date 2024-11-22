714400620b8ee6b1e2a49342371829fcdc46c55a
Soumyodeep-Das <linux.programmer.06@gmail.com>
2024-11-22 23:36:53 +0530

    largest element in an array

---
+++ largest_in_array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        // code here
+        int max=0;
+        for(int i=0; i<arr.size(); i++)
+        {
+            if(max<arr[i]){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+};
